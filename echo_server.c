0d5b570505590dde04c35e0dd2c7df9eaaece9ef
EthanK28 <kes0228@gmail.com>
2016-02-27 11:41:46 +0900

    Init

---
+++ echo_server.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
+#include<arpa/inet.h>
+#include<sys/socket.h>
+
+#define BUF_SIZE 1024
+void error_handling(char *message);
+
+int main(int argc, char *argv[])
+{
+	int serv_sock, clnt_sock;
+	char message[BUF_SIZE];
+	int str_len, i;
+
+	struct sockaddr_in srv_adr, clnt_adr;
+	socklen_t clnt_adr_sz;
+	
+
